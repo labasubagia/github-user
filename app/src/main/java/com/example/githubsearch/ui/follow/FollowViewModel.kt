a57cf7f3fe54f10e0b7f7bf78f372cbfc9171ff2
Laba Subagia <labasubagia22@gmail.com>
2020-06-01 23:52:25 +0800

    add follow fragment view model

---
+++ app/src/main/java/com/example/githubsearch/ui/follow/FollowViewModel.kt
@@ -0,0 +1,70 @@
+package com.example.githubsearch.ui.follow
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.githubsearch.model.User
+import com.example.githubsearch.network.NetworkConfig
+import com.example.githubsearch.util.Util
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class FollowViewModel : ViewModel() {
+
+
+    private val userFollowers = MutableLiveData<ArrayList<User>>()
+    private val userFollowing = MutableLiveData<ArrayList<User>>()
+    private val errorMessageInt = MutableLiveData<Int>()
+
+    // request user's followers
+    fun setUserFollower(username: String) {
+        NetworkConfig().api().userFollowers(username).enqueue(object : Callback<ArrayList<User>> {
+            override fun onFailure(call: Call<ArrayList<User>>, t: Throwable) {
+                errorMessageInt.value =
+                    Util.getRequestErrorResourceInt(Util.REQUEST_ERROR_NETWORK_FAILURE)
+            }
+
+            override fun onResponse(
+                call: Call<ArrayList<User>>,
+                response: Response<ArrayList<User>>
+            ) {
+                if (response.isSuccessful) {
+                    errorMessageInt.value = null
+                    userFollowers.value = response.body()
+                } else {
+                    errorMessageInt.value =
+                        Util.getRequestErrorResourceInt(Util.REQUEST_ERROR_API_PROBLEM)
+                }
+            }
+        })
+    }
+
+    // request user's following
+    fun setUserFollowing(username: String) {
+        NetworkConfig().api().userFollowing(username).enqueue(object : Callback<ArrayList<User>> {
+            override fun onFailure(call: Call<ArrayList<User>>, t: Throwable) {
+                errorMessageInt.value =
+                    Util.getRequestErrorResourceInt(Util.REQUEST_ERROR_NETWORK_FAILURE)
+            }
+
+            override fun onResponse(
+                call: Call<ArrayList<User>>,
+                response: Response<ArrayList<User>>
+            ) {
+                if (response.isSuccessful) {
+                    errorMessageInt.value = null
+                    userFollowing.value = response.body()
+                } else {
+                    errorMessageInt.value =
+                        Util.getRequestErrorResourceInt(Util.REQUEST_ERROR_API_PROBLEM)
+                }
+            }
+        })
+    }
+
+    fun getUserFollowers() = userFollowers
+
+    fun getUserFollowing() = userFollowing
+
+    fun getErrorMessageInt() = errorMessageInt
+}
